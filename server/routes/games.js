67187d122e1c91fa82ad85496b75591af811f750
Myrie2005 <myrie.f.2005@gmail.com>
2026-01-06 17:59:40 +0200

    small update

--- server/routes/games.js
+++ server/routes/games.js
@@ -5,7 +5,7 @@ import { fileURLToPath } from "url";
 
 const router = express.Router();
 
-// בשביל path ב־ESM
+
 const __filename = fileURLToPath(
     import.meta.url);
 const __dirname = path.dirname(__filename);
